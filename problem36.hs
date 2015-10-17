768d36ff9efe3a414237e4134d3e495d7c460fa9
Ruben Vereecken <rubenvereecken@gmail.com>
2015-10-18 00:01:02 +0200

    Added some comments for problem 36 and 37

--- problem36.hs
+++ problem36.hs
@@ -1,6 +1,7 @@
 import Numeric (showIntAtBase)
 import Data.Char (intToDigit)
 
+# Even after implementing this a hundred times
 isPalindrome :: String -> Bool
 isPalindrome "" = True
 isPalindrome [s] = True
@@ -9,6 +10,7 @@ isPalindrome (h:t) = (h == last t) && (isPalindrome $ init t)
 toBinary :: Int -> String
 toBinary n = showIntAtBase 2 intToDigit n ""
 
+# A number is a special palindrome if it's a palindrome it base 2 and 10
 isSpecialPalindrome :: Int -> Bool
 isSpecialPalindrome x = if (isPalindrome . show) x && (isPalindrome . toBinary) x then True else False
 
--- problem37.hs
+++ problem37.hs
@@ -1,8 +1,10 @@
 import Data.Numbers.Primes
 
 allThePrimes = primes
+# Single-digit primes were not considered
 almostAllThePrimes = filter (>10) allThePrimes
 
+# These two truncatable prime functions could have been prettier
 isTruncatablePrimeLeft :: String -> Bool
 isTruncatablePrimeLeft "" = True
 isTruncatablePrimeLeft whole@(_:t) = (isPrime (read whole :: Integer)) && isTruncatablePrimeLeft t
@@ -11,6 +13,7 @@ isTruncatablePrimeRight :: String -> Bool
 isTruncatablePrimeRight "" = True
 isTruncatablePrimeRight whole = (isPrime (read whole :: Integer)) && (isTruncatablePrimeRight $ init whole)
 
+# Call as answer 0 almostAllThePrimes
 answer :: Int -> [Integer] -> Integer
 answer 11 sieve = 0
 answer n (h:t) = let stringified = show h
